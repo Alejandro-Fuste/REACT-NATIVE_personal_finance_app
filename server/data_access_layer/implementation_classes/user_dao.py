d6e9b11f76b09cfcf28703c84b32fc50e2556238
Alejandro F <afuste7@gmail.com>
2022-03-22 17:17:01 -0400

    added abstract methods

---
+++ server/data_access_layer/implementation_classes/user_dao.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from server.data_access_layer.abstract_classes.user_dao import UserDAO
+from server.entities.user import User
+
+
+class UserDAOImp(UserDAO):
+    def create_new_user(self, user: User) -> User:
+        pass
+
+    def get_user(self, user_id: int) -> User:
+        pass
+
+    def get_all_users(self) -> List[User]:
+        pass
+
+    def update_user(self, user_id: int, user: User) -> User:
+        pass
+
+    def delete_user(self, user_id: int) -> bool:
+        pass
