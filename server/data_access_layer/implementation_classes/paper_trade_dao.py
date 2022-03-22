bba4d44bfb6ae2620de2ff355ea083cc3bd4e38f
Alejandro F <afuste7@gmail.com>
2022-03-22 17:22:34 -0400

    added code to paper trade dao and added environment variables file to git ignore

---
+++ server/data_access_layer/implementation_classes/paper_trade_dao.py
@@ -0,0 +1,13 @@
+from server.data_access_layer.abstract_classes.paper_trade_dao import PaperTradeDAO
+from server.entities.paper_trade import PaperTrade
+
+
+class PaperTradeDAOImp(PaperTradeDAO):
+    def add_paper_trade(self, paper_trade: PaperTrade) -> PaperTrade:
+        pass
+
+    def update_paper_trade(self, paper_trade_id: int, paper_trade: PaperTrade) -> PaperTrade:
+        pass
+
+    def delete_user(self, paper_trade_id: int) -> bool:
+        pass
