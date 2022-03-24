7e112c5bf6298244d9eac3b7ceab53ea6f49ac7c
Alejandro F <afuste7@gmail.com>
2022-03-24 14:59:22 -0400

    removed id property

--- server/entities/user.py
+++ server/entities/user.py
@@ -1,6 +1,5 @@
 class User:
     def __init__(self,
-                 user_id: int = None,
                  first_name: str = None,
                  last_name: str = None,
                  email: str = None,
@@ -8,7 +7,6 @@ class User:
                  password: str = None,
                  paper_trade: dict = {}
                  ):
-        self.user_id = user_id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
@@ -18,7 +16,6 @@ class User:
 
     def make_dictionary(self):
         dictionary = {
-            "userId": self.user_id,
             "firstName": self.first_name,
             "lastName": self.last_name,
             "email": self.email,
@@ -29,6 +26,7 @@ class User:
         return dictionary
 
     def __str__(self):
-        return f"user_id: {self.user_id}, first_name: {self.first_name}, last_name: {self.last_name}, " \
-               f"email: {self.email}, username: {self.username}, passcode: {self.password}, paper_trade: {self.paper_trade}"
+        return f"first_name: {self.first_name}, last_name: {self.last_name}, " \
+               f"email: {self.email}, username: {self.username}, passcode: {self.password}, " \
+               f"paper_trade: {self.paper_trade}"
 
