d5182af77d6dc26667dd8b9259755a3f3fea5839
Alejandro F <afuste7@gmail.com>
2022-03-24 15:23:09 -0400

    added random faker int for id of paper trade

--- server/tests/data_access_layer/conftest.py
+++ server/tests/data_access_layer/conftest.py
@@ -9,13 +9,14 @@ fake = Faker()
 
 @fixture
 def create_new_user() -> User:
-    return User(0, fake.first_name(), fake.last_name(), fake.ascii_company_email(), fake.domain_word(),
+    return User(fake.first_name(), fake.last_name(), fake.ascii_company_email(), fake.domain_word(),
                 fake.sha256(raw_output=False))
 
 
 @fixture
 def create_new_paper_trade() -> PaperTrade:
-    return PaperTrade(1, "T", fake.pyfloat(left_digits=2, right_digits=2, positive=True), "Call", "3/25", "Straddle",
+    return PaperTrade(fake.pyint(max_value=10000), "T", fake.pyfloat(left_digits=2, right_digits=2, positive=True),
+                      "Call", "3/25", "Straddle",
                       fake.pyfloat(left_digits=0, right_digits=2, positive=True),
                       fake.pyfloat(left_digits=0, right_digits=2, positive=True),
                       fake.pyfloat(left_digits=2, right_digits=2, positive=True),
