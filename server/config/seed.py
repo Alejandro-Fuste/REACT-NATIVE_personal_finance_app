9191c76c5b0756351c66ac61907cf6cabd84d648
Alejandro F <afuste7@gmail.com>
2022-03-24 14:58:14 -0400

    changed seed data

--- server/config/seed.py
+++ server/config/seed.py
@@ -19,7 +19,7 @@ print(f"{delete_previous_entries.deleted_count} documents deleted.")
 fake = Faker()
 
 #   Paper Trades --------------------
-paper_trade_1 = PaperTrade(fake.uuid4(cast_to=int), "T", fake.pyfloat(left_digits=2, right_digits=2, positive=True),
+paper_trade_1 = PaperTrade(1, "T", fake.pyfloat(left_digits=2, right_digits=2, positive=True),
                            "Call", "3/25", "Straddle",
                            fake.pyfloat(left_digits=0, right_digits=2, positive=True),
                            fake.pyfloat(left_digits=0, right_digits=2, positive=True),
@@ -36,12 +36,12 @@ paper_trade_1 = PaperTrade(fake.uuid4(cast_to=int), "T", fake.pyfloat(left_digit
 
 #   Users  --------------------------
 
-test_user_1 = User(fake.uuid4(cast_to=int), "Luke", "Skywalker", "luke.skywalker@jedi.com",
+test_user_1 = User(0, "Luke", "Skywalker", "luke.skywalker@jedi.com",
                    "Master Luke", fake.sha256(raw_output=False), paper_trade_1.make_dictionary())
 
 test_user_2 = User(fake.uuid4(cast_to=int), "Leia", "Organa", "leia.organa@jedi.net",
                    "Princess Leia", fake.sha256(raw_output=False))
 
-result = collection.insert_one(test_user_1)
+result = collection.insert_one(test_user_1.make_dictionary())
 
 print(result.inserted_id)
