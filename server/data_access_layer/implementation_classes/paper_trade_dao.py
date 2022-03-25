f24e549aedf3fb4574c698d98db3443935144e23
Alejandro F <afuste7@gmail.com>
2022-03-25 18:13:35 -0400

    removed commented out code

--- server/data_access_layer/implementation_classes/paper_trade_dao.py
+++ server/data_access_layer/implementation_classes/paper_trade_dao.py
@@ -1,10 +1,7 @@
-from pprint import pprint
-
 from pymongo import MongoClient
 from environment_variables import mongo_url
 from server.data_access_layer.abstract_classes.paper_trade_dao import PaperTradeDAO
 from server.entities.paper_trade import PaperTrade
-from typing import List
 from bson.objectid import ObjectId
 
 # database connection -------------
@@ -35,13 +32,3 @@ class PaperTradeDAOImp(PaperTradeDAO):
         deleted = collection.update_one({"_id": ObjectId(user_id)},
                                         {"$pull": {"paperTrades": {"tradeId": paper_trade_id}}})
         return deleted.acknowledged
-
-
-# deleted = collection.update_one({"_id": ObjectId("623ddc582a8e2cee29b0b62d")},
-# {"$pull":{"paperTrades": {"tradeId": 7721}}})
-# print(deleted.acknowledged)
-
-# users[0]["paperTrades"][-1]["tradeId"]
-# users = collection.find({"_id": ObjectId("623ddc582a8e2cee29b0b62d")})
-# for user in users:
-#     pprint(user["paperTrades"][-1]["tradeId"])
