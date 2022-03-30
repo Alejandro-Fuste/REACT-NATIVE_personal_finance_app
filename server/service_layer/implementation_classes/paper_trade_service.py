30e907268e8c933711d36568e950746bd97e5d40
Alejandro F <afuste7@gmail.com>
2022-03-30 13:40:33 -0400

    added methods from abs class

--- server/service_layer/implementation_classes/paper_trade_service.py
+++ server/service_layer/implementation_classes/paper_trade_service.py
@@ -1,2 +1,23 @@
 from server.data_access_layer.implementation_classes.paper_trade_dao import PaperTradeDAOImp
+from server.entities.paper_trade import PaperTrade
 from server.service_layer.abstract_classes.paper_trade_service_abs import PaperTradeService
+
+
+class PaperTradeServiceImp(PaperTradeService):
+    def __init__(self, paper_trade_dao):
+        self.paper_trade_dao: PaperTradeDAOImp = paper_trade_dao
+        
+    def add_paper_trade(self, user_id: str, paper_trade: PaperTrade) -> dict:
+        pass
+
+    def get_paper_trades(self, user_id: str) -> list:
+        pass
+
+    def update_paper_trade_sell_price(self, user_id: str, paper_trade_index: int, sell_price: float) -> bool:
+        pass
+
+    def delete_paper_trade(self, user_id: str, paper_trade_id: int) -> int:
+        pass
+
+
+
