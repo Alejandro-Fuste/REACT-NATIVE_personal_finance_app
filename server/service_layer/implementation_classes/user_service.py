8c7873a113c28b087c5cc6b1f38ab3409a685102
Alejandro F <afuste7@gmail.com>
2022-03-30 16:34:26 -0400

    added get user by id service test with magic mock

--- server/service_layer/implementation_classes/user_service.py
+++ server/service_layer/implementation_classes/user_service.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from ser
+
 from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
 from server.entities.user import User
 from server.service_layer.abstract_classes.user_service_abs import UserService
--- server/tests/service_layer/test_user_service.py
+++ server/tests/service_layer/test_user_service.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock, patch, Mock
 
 from server.custom_exceptions.user_id_must_be_string import UserIdMustBeString
+from server.custom_exceptions.user_id_not_provided import MissingUserId
 
 from server.data_access_layer.abstract_classes.user_dao import UserDAO
 from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
@@ -10,7 +11,8 @@ from server.service_layer.implementation_classes.user_service import UserService
 user_dao: UserDAO = UserDAOImp()
 user_service: UserService = UserServiceImp(user_dao)
 
-user_id_must_be_string: str = "The user id must be a string"
+user_id_must_be_string: str = "The user id must be a string."
+user_id_not_provided: str = "A user id must be provided."
 
 
 # Creation Tests --------------------------------------
@@ -20,14 +22,27 @@ user_id_must_be_string: str = "The user id must be a string"
 # ID is integer instead of string
 def test_get_user_by_id_not_string(invalid_id):
     try:
-        user_service.get_user_by_id(invalid_id)
+        user_service.get_user_by_id = Mock(side_effect=Exception(user_id_must_be_string))
+        assert False
     except UserIdMustBeString as e:
         assert str(e) == user_id_must_be_string
 
 
+# def test_get_user_by_id_not_string(invalid_id):
+#     try:
+#         user_service.get_user_by_id(invalid_id)
+#         assert False
+#     except UserIdMustBeString as e:
+#         assert str(e) == user_id_must_be_string
+
+
 # ID was not provided
-def test_get_user_by_id_no_id():
-    pass
+def test_get_user_by_id_no_id(missing_id):
+    try:
+        user_service.get_user_by_id(missing_id)
+        assert False
+    except MissingUserId as e:
+        assert str(e) == user_id_not_provided
 
 # Update Tests ----------------------------------------
 
