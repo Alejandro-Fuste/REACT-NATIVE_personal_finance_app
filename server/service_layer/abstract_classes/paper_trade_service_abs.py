ab33cc1911bb5fe7f4ee2de07c46a7cb08034dc0
Alejandro F <afuste7@gmail.com>
2022-03-30 12:56:35 -0400

    added code to service paper trade abstract file

---
+++ server/service_layer/abstract_classes/paper_trade_service_abs.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+from server.entities.paper_trade import PaperTrade
+
+
+class PaperTradeDAO(ABC):
+
+    # Create method -------
+    @abstractmethod
+    def add_paper_trade(self, user_id: str, paper_trade: PaperTrade) -> dict:
+        pass
+
+    # Read methods --------
+    @abstractmethod
+    def get_paper_trades(self, user_id: str) -> list:
+        pass
+
+    # Update method -------
+    @abstractmethod
+    def update_paper_trade_sell_price(self, user_id: str, paper_trade_index: int, sell_price: float) -> bool:
+        pass
+
+    # Delete method -------
+    @abstractmethod
+    def delete_paper_trade(self, user_id: str, paper_trade_id: int) -> int:
+        pass
