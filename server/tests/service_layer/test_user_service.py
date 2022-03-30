430d5df47f0bd0a7be575ab1328219d8e0cd454d
Alejandro F <afuste7@gmail.com>
2022-03-30 14:52:10 -0400

    added service imports, user dao, user service variables

--- server/tests/service_layer/test_user_service.py
+++ server/tests/service_layer/test_user_service.py
@@ -1,7 +1,17 @@
 from unittest.mock import MagicMock, patch, Mock
 
+from server.custom_exceptions.user_not_found import UserNotFound
+from server.custom_exceptions.duplicate_user import DuplicateUser
+
 from server.data_access_layer.abstract_classes.user_dao import UserDAO
 from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
 
-from server.custom_exceptions.user_not_found import UserNotFound
-from server.custom_exceptions.duplicate_user import DuplicateUser
+from server.service_layer.implementation_classes.user_service import UserService, UserServiceImp
+
+user_dao: UserDAO = UserDAOImp()
+user_service: UserService = UserServiceImp(user_dao)
+
+
+
+
+
