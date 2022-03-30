e9d82dcf3e0d97e432215799a6fa6449da9861fc
Alejandro F <afuste7@gmail.com>
2022-03-30 15:34:21 -0400

    added code for user id is not a string test

--- server/tests/service_layer/test_user_service.py
+++ server/tests/service_layer/test_user_service.py
@@ -10,16 +10,19 @@ from server.service_layer.implementation_classes.user_service import UserService
 user_dao: UserDAO = UserDAOImp()
 user_service: UserService = UserServiceImp(user_dao)
 
+user_id_must_be_string: str = "The user id must be a string"
+
+
 # Creation Tests --------------------------------------
 
 # Read Tests ------------------------------------------
+
 # ID is integer instead of string
-
-def test_get_user_by_id_not_string():
+def test_get_user_by_id_not_string(invalid_id):
     try:
-        pass
-    except 
-
+        user_service.get_user_by_id(invalid_id)
+    except UserIdMustBeString as e:
+        assert str(e) == user_id_must_be_string
 
 # Update Tests ----------------------------------------
 
