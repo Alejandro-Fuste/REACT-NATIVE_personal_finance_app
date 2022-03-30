ba76c8036f318ac3f762f81c09bfaf83405f677e
Alejandro F <afuste7@gmail.com>
2022-03-30 14:47:13 -0400

    added exceptions imports

--- server/tests/service_layer/test_user_service.py
+++ server/tests/service_layer/test_user_service.py
@@ -2,3 +2,6 @@ from unittest.mock import MagicMock, patch, Mock
 
 from server.data_access_layer.abstract_classes.user_dao import UserDAO
 from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
+
+from server.custom_exceptions.user_not_found import UserNotFound
+from server.custom_exceptions.duplicate_user import DuplicateUser
