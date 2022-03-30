c32e855f43b77acc0738fe3a6d92de1ce9c225dd
Alejandro F <afuste7@gmail.com>
2022-03-30 13:27:07 -0400

    added methods from abs class

--- server/service_layer/implementation_classes/user_service.py
+++ server/service_layer/implementation_classes/user_service.py
@@ -1,2 +1,30 @@
+from typing import List
+
 from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
+from server.entities.user import User
 from server.service_layer.abstract_classes.user_service_abs import UserService
+
+
+class UserServiceImp(UserService):
+    def __init__(self, user_dao):
+        self.user_dao: UserDAOImp = user_dao
+
+    def create_new_user(self, user: User) -> dict:
+        pass
+
+    def get_user_by_id(self, user_id: str) -> dict:
+        pass
+
+    def get_user_by_username(self, username: str) -> dict:
+        pass
+
+    def get_all_users(self) -> List[User]:
+        pass
+
+    def update_username(self, user_id: str, new_info: str) -> dict:
+        pass
+
+    def delete_user(self, user_id: str) -> int:
+        pass
+
+
