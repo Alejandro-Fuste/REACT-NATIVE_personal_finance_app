cd945e9bcd41c863a084c416af0419394b4f6290
Alejandro F <afuste7@gmail.com>
2022-03-30 12:55:21 -0400

    added code to service user abstract file

---
+++ server/service_layer/abstract_classes/user_service_abs.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from server.entities.user import User
+
+
+class UserDAO(ABC):
+
+    # Create method -------
+    @abstractmethod
+    def create_new_user(self, user: User) -> dict:
+        pass
+
+    # Read methods -------
+    @abstractmethod
+    def get_user_by_id(self, user_id: str) -> dict:
+        pass
+
+    @abstractmethod
+    def get_user_by_username(self, username: str) -> dict:
+        pass
+
+    @abstractmethod
+    def get_all_users(self) -> List[User]:
+        pass
+
+    # Update method -------
+    @abstractmethod
+    def update_username(self, user_id: str, new_info: str) -> dict:
+        pass
+
+    # Delete method -------
+    @abstractmethod
+    def delete_user(self, user_id: str) -> int:
+        pass
