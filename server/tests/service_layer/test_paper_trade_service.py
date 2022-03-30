f48f1b8e433e20ba9e773f6f8c09f7a970b47a8b
Alejandro F <afuste7@gmail.com>
2022-03-30 12:52:48 -0400

    added imports to test user and paper trade service

---
+++ server/tests/service_layer/test_paper_trade_service.py
@@ -0,0 +1,4 @@
+from unittest.mock import MagicMock, patch, Mock
+
+from server.data_access_layer.abstract_classes.paper_trade_dao import PaperTradeDAO
+from server.data_access_layer.implementation_classes.paper_trade_dao import PaperTradeDAOImp
---
+++ server/tests/service_layer/test_user_service.py
@@ -0,0 +1,4 @@
+from unittest.mock import MagicMock, patch, Mock
+
+from server.data_access_layer.abstract_classes.user_dao import UserDAO
+from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
