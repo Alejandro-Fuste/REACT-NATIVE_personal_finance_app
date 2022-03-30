f2634991b28b4307ac7036efbee3e2d4919b8d02
Alejandro F <afuste7@gmail.com>
2022-03-30 15:29:53 -0400

    added new exception to custom exceptions file

---
+++ server/custom_exceptions/user_id_must_be_string.py
@@ -0,0 +1,3 @@
+class UserIdMustBeString(Exception):
+    def __init__(self, message):
+        self.message = message
--- server/tests/service_layer/test_user_service.py
+++ server/tests/service_layer/test_user_service.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock, patch, Mock
 
-from server.custom_exceptions.user_not_found import UserNotFound
-from server.custom_exceptions.duplicate_user import DuplicateUser
+from server.custom_exceptions.user_id_must_be_string import UserIdMustBeString
 
 from server.data_access_layer.abstract_classes.user_dao import UserDAO
 from server.data_access_layer.implementation_classes.user_dao import UserDAOImp
@@ -11,17 +10,17 @@ from server.service_layer.implementation_classes.user_service import UserService
 user_dao: UserDAO = UserDAOImp()
 user_service: UserService = UserServiceImp(user_dao)
 
-
 # Creation Tests --------------------------------------
 
 # Read Tests ------------------------------------------
+# ID is integer instead of string
+
+def test_get_user_by_id_not_string():
+    try:
+        pass
+    except 
+
 
 # Update Tests ----------------------------------------
 
 # Delete Tests ----------------------------------------
-
-
-
-
-
-
