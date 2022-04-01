5489dd6a720d3463a65aef9982110a9873a71070
Alejandro F <afuste7@gmail.com>
2022-04-01 12:19:29 -0400

    added user dao import and variable to get users

--- server/tests/service_layer/test_paper_trade_service.py
+++ server/tests/service_layer/test_paper_trade_service.py
@@ -1,10 +1,18 @@
+from server.custom_exceptions.user_id_must_be_string import UserIdMustBeString
+from server.custom_exceptions.user_id_not_provided import MissingUserId
+
+from server.data_access_layer.implementation_classes.user_dao import UserDAO, UserDAOImp
 from server.data_access_layer.abstract_classes.paper_trade_dao import PaperTradeDAO
 from server.data_access_layer.implementation_classes.paper_trade_dao import PaperTradeDAOImp
 
 from server.service_layer.implementation_classes.paper_trade_service import PaperTradeService, PaperTradeServiceImp
 
+user_dao: UserDAO = UserDAOImp()
 paper_trade_dao: PaperTradeDAO = PaperTradeDAOImp()
 paper_trade_service: PaperTradeService = PaperTradeServiceImp(paper_trade_dao)
+
+user_id_must_be_string: str = "The user id must be a string."
+user_id_not_provided: str = "A user id must be provided."
 
 
 # Creation Tests ----------------------------------------------------------------------------
@@ -13,6 +21,7 @@ paper_trade_service: PaperTradeService = PaperTradeServiceImp(paper_trade_dao)
 # Delete Tests ------------------------------------------------------------------------------
 # id not string
 def test_delete_paper_trade_id_not_string(invalid_id):
+    users: list = user_dao.get_all_users()
     try:
         paper_trade_service.delete_paper_trade(invalid_id)
         assert False
