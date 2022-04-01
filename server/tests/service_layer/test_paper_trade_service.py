765cf991c77c0fb4100a8add4f1c6cc6f629e22d
Alejandro F <afuste7@gmail.com>
2022-04-01 14:18:18 -0400

    added delete paper trade test

--- server/tests/service_layer/test_paper_trade_service.py
+++ server/tests/service_layer/test_paper_trade_service.py
@@ -1,5 +1,6 @@
 from server.custom_exceptions.user_id_must_be_string import UserIdMustBeString
 from server.custom_exceptions.user_id_not_provided import MissingUserId
+from server.custom_exceptions.paper_trade_exception import PaperTradeException
 
 from server.data_access_layer.implementation_classes.user_dao import UserDAO, UserDAOImp
 from server.data_access_layer.abstract_classes.paper_trade_dao import PaperTradeDAO
@@ -13,6 +14,8 @@ paper_trade_service: PaperTradeService = PaperTradeServiceImp(paper_trade_dao)
 
 user_id_must_be_string: str = "The user id must be a string."
 user_id_not_provided: str = "A user id must be provided."
+paper_trade_id_must_be_string: str = "The paper trade id must be a string."
+paper_trade_id_not_provided: str = "A paper trade id must be provided."
 
 
 # Creation Tests ----------------------------------------------------------------------------
@@ -20,7 +23,7 @@ user_id_not_provided: str = "A user id must be provided."
 # Update Tests ------------------------------------------------------------------------------
 # Delete Tests ------------------------------------------------------------------------------
 # id not string
-def test_delete_paper_trade_id_not_string(invalid_id):
+def test_delete_paper_trade_user_id_not_string(invalid_id):
     users: list = user_dao.get_all_users()
     trade_id: int = users[0]["paperTrades"][0]["tradeId"]
     try:
@@ -30,8 +33,18 @@ def test_delete_paper_trade_id_not_string(invalid_id):
         assert str(e) == user_id_must_be_string
 
 
+def test_delete_paper_trade_id_not_string(invalid_id):
+    users: list = user_dao.get_all_users()
+    user_id: str = users[0]["_id"]
+    try:
+        paper_trade_service.delete_paper_trade(user_id, invalid_id)
+        assert False
+    except PaperTradeException as e:
+        assert str(e) == paper_trade_id_must_be_string
+
+
 # id missing
-def test_delete_paper_trade_no_id(missing_id):
+def test_delete_paper_trade_no_user_id(missing_id):
     users: list = user_dao.get_all_users()
     trade_id: int = users[0]["paperTrades"][0]["tradeId"]
     try:
@@ -39,3 +52,13 @@ def test_delete_paper_trade_no_id(missing_id):
         assert False
     except MissingUserId as e:
         assert str(e) == user_id_not_provided
+
+
+def test_delete_paper_trade_no_id(missing_id):
+    users: list = user_dao.get_all_users()
+    user_id: str = users[0]["_id"]
+    try:
+        paper_trade_service.delete_paper_trade(user_id, missing_id)
+        assert False
+    except PaperTradeException as e:
+        assert str(e) == paper_trade_id_not_provided
