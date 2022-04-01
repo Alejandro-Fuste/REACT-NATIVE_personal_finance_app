2bd4dac176db201a5145fdd6c918728ce12b9a6d
Alejandro F <afuste7@gmail.com>
2022-04-01 11:50:49 -0400

    added variables for paper trade dao and paper trade service

--- server/tests/service_layer/test_paper_trade_service.py
+++ server/tests/service_layer/test_paper_trade_service.py
@@ -1,4 +1,9 @@
-from unittest.mock import MagicMock, patch, Mock
-
 from server.data_access_layer.abstract_classes.paper_trade_dao import PaperTradeDAO
 from server.data_access_layer.implementation_classes.paper_trade_dao import PaperTradeDAOImp
+
+from server.service_layer.implementation_classes.paper_trade_service import PaperTradeService, PaperTradeServiceImp
+
+paper_trade_dao: PaperTradeDAO = PaperTradeDAOImp()
+paper_trade_service: PaperTradeService = PaperTradeServiceImp(paper_trade_dao)
+
+
