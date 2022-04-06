b2246e2d23a9ef6fb9e5cb3a45bd9fbdca42dc2d
Alejandro F <afuste7@gmail.com>
2022-04-06 15:49:28 -0400

    commented out unusable imports

--- server/django_app/api/views.py
+++ server/django_app/api/views.py
@@ -1,23 +1,23 @@
+import sys
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+# from django.core.files import File
+#
+# import server.data_access_layer.implementation_classes.user_dao
+# from server.service_layer.implementation_classes.user_service import UserServiceImp
+#
+#
+# user_dao = UserDAOImp()
+# user_service = UserServiceImp(user_dao)
 
 
 # Create functions ------------------------------------------------------------
 # Read functions ------------------------------------------------------------
+
+# def get_all_users(request):
+#     data = user_service.get_all_users()
+#     return JsonResponse(data, safe=False, status=200)
 # Update functions ------------------------------------------------------------
 # Delete functions ------------------------------------------------------------
-
-
-def say_hello(request):
-    return HttpResponse("Hello World")
-
-
-def sample_data(request):
-    data = {
-        'name': 'Victor',
-        'location': 'Finland',
-        'is_active': True,
-        'count': 28
-    }
-    return JsonResponse(data, status=200)
