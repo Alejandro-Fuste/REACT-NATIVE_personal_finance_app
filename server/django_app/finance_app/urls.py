e2c5c2f835e99a2d21e69e4c99240be5a50e1304
Alejandro F <afuste7@gmail.com>
2022-04-07 12:06:37 -0400

    commented out url

--- server/django_app/finance_app/urls.py
+++ server/django_app/finance_app/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib import sitemaps
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # path(''),
     path('admin/', admin.site.urls),
     path('home/', views.get_all_users_controller),
     path('api/', include('api.urls'))
