4d8e366b9650dba4525c4a3b4d867b0a0ecf359b
Alejandro F <afuste7@gmail.com>
2022-04-07 16:26:10 -0400

    added initial user crud functions to urls

--- server/django_app/api/urls.py
+++ server/django_app/api/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('', views.create_new_user()),
-    path('', views.get_user_by_id()),
-    path('', views.get_user_by_username()),
-    path('', views.get_all_users()),
-    path('', views.update_username()),
-    path('', views.delete_user())
+    path('', views.create_new_user),
+    path('', views.get_user_by_id),
+    path('', views.get_user_by_username),
+    path('', views.get_all_users),
+    path('', views.update_username),
+    path('', views.delete_user)
 ]
