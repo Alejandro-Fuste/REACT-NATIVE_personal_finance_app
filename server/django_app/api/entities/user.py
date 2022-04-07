4c5c88a895ad15878fde981c31527044fbfa3260
Alejandro F <afuste7@gmail.com>
2022-04-07 14:34:11 -0400

    added _id property to User entity and changed password

--- server/django_app/api/entities/user.py
+++ server/django_app/api/entities/user.py
@@ -1,15 +1,14 @@
-from typing import List
-
-
 class User:
     def __init__(self,
+                 _id: str = None,
                  first_name: str = None,
                  last_name: str = None,
                  email: str = None,
                  username: str = None,
                  password: str = None,
-                 paper_trades: List = []
+                 paper_trades: list = []
                  ):
+        self._id = str(_id)
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
@@ -19,17 +18,18 @@ class User:
 
     def make_dictionary(self):
         dictionary = {
+            "_id": self._id,
             "firstName": self.first_name,
             "lastName": self.last_name,
             "email": self.email,
             "username": self.username,
-            "password": self.password,
+            "password": "*****",
             "paperTrades": self.paper_trades
         }
         return dictionary
 
     def __str__(self):
         return f"first_name: {self.first_name}, last_name: {self.last_name}, " \
-               f"email: {self.email}, username: {self.username}, passcode: {self.password}, " \
+               f"email: {self.email}, username: {self.username}, passcode: *****, " \
                f"paper_trade: {self.paper_trades}"
 
