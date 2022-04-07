81e9dd263964e34dcb8226608145babef300ff64
Alejandro F <afuste7@gmail.com>
2022-04-07 16:21:47 -0400

    added initial user crud functions to views

--- server/django_app/api/views.py
+++ server/django_app/api/views.py
@@ -1,9 +1,8 @@
-import sys
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-# from django.core.files import File
+
+
 #
 # import server.data_access_layer.implementation_classes.user_dao
 # from server.service_layer.implementation_classes.user_service import UserServiceImp
@@ -14,10 +13,28 @@ from django.http import JsonResponse
 
 
 # Create functions ------------------------------------------------------------
-# Read functions ------------------------------------------------------------
+def create_new_user(request):
+    pass
 
-# def get_all_users(request):
-#     data = user_service.get_all_users()
-#     return JsonResponse(data, safe=False, status=200)
+
+# Read functions ------------------------------------------------------------
+def get_user_by_id(request):
+    pass
+
+
+def get_user_by_username(request):
+    pass
+
+
+def get_all_users(request):
+    pass
+
+
 # Update functions ------------------------------------------------------------
+def update_username(request):
+    pass
+
+
 # Delete functions ------------------------------------------------------------
+def delete_user(request):
+    pass
