cfc0cee821dd4c3acdab465d544daf32cebc753c
Alejandro F <afuste7@gmail.com>
2022-04-07 11:31:00 -0400

    changed import in test paper trade dao

--- server/tests/data_access_layer/test_paper_trade_dao.py
+++ server/tests/data_access_layer/test_paper_trade_dao.py
@@ -1,5 +1,6 @@
-from server.django_app.api.data_access_layer.implementation_classes import UserDAOImp, UserDAO
-from server.django_app.api.data_access_layer.implementation_classes import PaperTradeDAOImp, PaperTradeDAO
+from server.django_app.api.data_access_layer.implementation_classes.user_dao import UserDAOImp, UserDAO
+from server.django_app.api.data_access_layer.implementation_classes.paper_trade_dao import PaperTradeDAOImp, \
+    PaperTradeDAO
 from server.django_app.api.custom_exceptions.duplicate_trade import DuplicateTrade
 from server.django_app.api.custom_exceptions.user_not_found import UserNotFound
 from server.django_app.api.custom_exceptions.trade_not_found import TradeNotFound
