155257b4f6cacb61254e8fe904c0044d4627be10
Alejandro F <afuste7@gmail.com>
2022-04-08 12:25:56 -0400

    added log config line and run command...test home get route worked

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__)
+logging.basicConfig(filename="records.log", level=logging.DEBUG,
+                    format="[%(levelname)s] - %(asctime)s - %(name)s - : %(message)s in %(pathname)s:%(lineno)d")
+
+app: Flask = Flask(__name__)
 CORS(app)
 
 
 @app.route("/")
-def helloWorld():
+def hello_world():
     return "Hello, cross-origin-world!"
+
 
 # data_list = []
 # dictionary_data = []
@@ -20,3 +24,5 @@ def helloWorld():
 # for d in data_list:
 #     dictionary_data.append(d.make_dictionary())
 # return dictionary_data
+
+app.run()
