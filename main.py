3b818458ac87a70d06da652909c0f852ee2c0eb6
Alejandro F <afuste7@gmail.com>
2022-04-08 12:17:36 -0400

    install flask_cors and added hello world route

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import logging
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 
-
-
+@app.route("/")
+def helloWorld():
+    return "Hello, cross-origin-world!"
 
 # data_list = []
-        # dictionary_data = []
-        # for item in data:
-        #     data_list.append(User(str(item["_id"]), item["firstName"], item["lastName"], item["email"],
-        #                           item["username"], item["password"], item['paperTrades']))
-        #
-        # for d in data_list:
-        #     dictionary_data.append(d.make_dictionary())
-        # return dictionary_data
-
+# dictionary_data = []
+# for item in data:
+#     data_list.append(User(str(item["_id"]), item["firstName"], item["lastName"], item["email"],
+#                           item["username"], item["password"], item['paperTrades']))
+#
+# for d in data_list:
+#     dictionary_data.append(d.make_dictionary())
+# return dictionary_data
