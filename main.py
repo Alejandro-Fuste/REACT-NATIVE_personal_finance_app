2cf0d07b9730cfafb20f536f6befdb70eec4dbae
Alejandro F <afuste7@gmail.com>
2022-04-08 14:05:25 -0400

    added imports for custom exceptions, dao, service layer, and entities

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import logging
+from server.custom_exceptions.duplicate_trade import DuplicateTrade
+from server.custom_exceptions.duplicate_user import DuplicateUser
+from server.custom_exceptions.email_wrong_format import EmailWrongFormat
+from server.custom_exceptions.input_missing import InputMissing
+from server.custom_exceptions.input_not_string import InputNotString
+from server.custom_exceptions.input_too_long import InputTooLong
+from server.custom_exceptions.input_too_short import InputTooShort
+from server.custom_exceptions.no_trades import NoTrades
+from server.custom_exceptions.paper_trade_exception import PaperTradeException
+from server.custom_exceptions.trade_not_found import TradeNotFound
+from server.custom_exceptions.user_id_not_provided import MissingUserId
+from server.custom_exceptions.user_id_must_be_string import UserIdMustBeString
+from server.custom_exceptions.user_not_found import UserNotFound
+
+from server.data_access_layer.implementation_classes.user_dao import UserDAO, UserDAOImp
+from server.data_access_layer.implementation_classes.paper_trade_dao import PaperTradeDAO, PaperTradeDAOImp
+
+from server.service_layer.implementation_classes.user_service import UserService, UserServiceImp
+from server.service_layer.implementation_classes.paper_trade_service import PaperTradeService, PaperTradeServiceImp
+
+from server.entities.db_user import DatabaseUser
+from server.entities.user import User
+from server.entities.paper_trade import PaperTrade
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-logging.basicConfig(filename="records.log", level=logging.DEBUG,
-                    format="[%(levelname)s] - %(asctime)s - %(name)s - : %(message)s in %(pathname)s:%(lineno)d")
+import logging
+logging.basicConfig(filename="records.log", level=logging.DEBUG, format=f"%(asctime)s %(levelname)s %(message)s")
 
 app: Flask = Flask(__name__)
 CORS(app)
 
 
 @app.route("/")
-def hello_world():
-    return "Hello, cross-origin-world!"
+def on():
+    return "Python is running!"
 
 
 # data_list = []
