4518461572550cf47156c76fbb928cd89ddfc69f
Alejandro F <afuste7@gmail.com>
2022-04-19 23:15:19 -0400

    added code for option dao methods...happy tests passed

--- server/data_access_layer/implementation_classes/options_info_dao.py
+++ server/data_access_layer/implementation_classes/options_info_dao.py
@@ -3,24 +3,25 @@ import pandas
 from server.data_access_layer.abstract_classes.options_info_dao import OptionsInfoDAO
 
 from yahoo_fin import options
+from yahoo_fin import stock_info
 
 
 class OptionsInfoImp(OptionsInfoDAO):
 
     def get_stock_price(self, ticker: str) -> float:
-        pass
+        return stock_info.get_live_price(ticker)
 
     def get_calls(self, ticker: str) -> pandas:
-        pass
+        return options.get_calls(ticker)
 
     def get_puts(self, ticker: str) -> pandas:
-        pass
+        return options.get_puts(ticker)
 
 
-calls = options.get_calls("t")
-# print(isinstance(calls.loc[4, "Strike"], float))
-print(type(calls))
-print(isinstance(calls, pandas))
+# price = stock_info.get_live_price("t")
+# # print(isinstance(calls.loc[4, "Strike"], float))
+# print(float(price))
+
 
 # for i in range(len(calls)):
 #     print(calls.loc[i, "Strike"])
