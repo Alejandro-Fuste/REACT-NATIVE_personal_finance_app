e7b84f0d8140c06a3680a1bbfee832023698eef9
Alejandro F <afuste7@gmail.com>
2022-04-19 22:50:55 -0400

    changed options dao abstract and imp files

--- server/data_access_layer/abstract_classes/options_info_dao.py
+++ server/data_access_layer/abstract_classes/options_info_dao.py
@@ -1,12 +1,19 @@
 from abc import ABC, abstractmethod
 
+import pandas
+
 
 class OptionsInfoDAO(ABC):
 
     @abstractmethod
-    def get_live_stock_price(self, ticker: str) -> float:
+    def get_stock_price(self, ticker: str) -> float:
         pass
 
     @abstractmethod
-    def get_options(self, ticker: str) -> dict:
+    def get_calls(self, ticker: str) -> pandas:
         pass
+
+    @abstractmethod
+    def get_puts(self, ticker: str) -> pandas:
+        pass
+
--- server/data_access_layer/implementation_classes/options_info_dao.py
+++ server/data_access_layer/implementation_classes/options_info_dao.py
@@ -1,24 +1,25 @@
-from pprint import pprint
+import pandas
 
 from server.data_access_layer.abstract_classes.options_info_dao import OptionsInfoDAO
-from server.environment_variables import api_key
-import requests
 
-# api connection info ----------------------------------------------
-url = "https://yh-finance.p.rapidapi.com/stock/v2/get-options"
-headers = {
-    "X-RapidAPI-Host": "yh-finance.p.rapidapi.com",
-    "X-RapidAPI-Key": api_key
-}
+from yahoo_fin import options
 
 
 class OptionsInfoImp(OptionsInfoDAO):
 
-    def get_live_stock_price(self, ticker: str) -> float:
+    def get_stock_price(self, ticker: str) -> float:
         pass
 
-    def get_options(self, ticker: str) -> dict:
-        querystring = {"symbol": ticker, "date": "1562284800", "region": "US"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        return response.json()
+    def get_calls(self, ticker: str) -> pandas:
+        pass
 
+    def get_puts(self, ticker: str) -> pandas:
+        pass
+
+
+calls = options.get_calls("t")
+# print(isinstance(calls.loc[4, "Strike"], float))
+# print(len(calls))
+
+# for i in range(len(calls)):
+#     print(calls.loc[i, "Strike"])
