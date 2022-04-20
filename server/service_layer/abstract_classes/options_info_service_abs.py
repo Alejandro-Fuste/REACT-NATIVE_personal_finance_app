f831b651c88354153294d3ded2516f6ae732a6a6
Alejandro F <afuste7@gmail.com>
2022-04-20 15:42:31 -0400

    added options info service abs file

---
+++ server/service_layer/abstract_classes/options_info_service_abs.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+import pandas
+
+
+class OptionsInfoDAO(ABC):
+
+    @abstractmethod
+    def get_stock_price(self, ticker: str) -> float:
+        pass
+
+    @abstractmethod
+    def get_calls(self, ticker: str) -> pandas:
+        pass
+
+    @abstractmethod
+    def get_puts(self, ticker: str) -> pandas:
+        pass
+
