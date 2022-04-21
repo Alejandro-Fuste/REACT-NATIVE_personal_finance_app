ff08466305055e23d433580ee48a7b904b3aeb79
Alejandro F <afuste7@gmail.com>
2022-04-21 11:07:52 -0400

    added code for options service tests

--- server/tests/service_layer/test_options_info_service.py
+++ server/tests/service_layer/test_options_info_service.py
@@ -1,14 +1,22 @@
+from server.custom_exceptions.input_not_string import InputNotString
+
 from server.data_access_layer.implementation_classes.options_info_dao import OptionsInfoDAO, OptionsInfoImp
 from server.service_layer.implementation_classes.options_info_service import OptionsInfoService, OptionsInfoServiceImp
 
 options_info_dao: OptionsInfoDAO = OptionsInfoImp()
 options_info_service: OptionsInfoService = OptionsInfoServiceImp(options_info_dao)
 
+input_must_be_string: str = "The input must be a string."
+
 
 # Read Tests --------------------------------------------------------------------------------
 # Get Stock Price Tests ---------------------------------------------------------------------
-def test_get_stock_price_ticker_not_string():
-    pass
+def test_get_stock_price_ticker_not_string(int_ticker):
+    try:
+        options_info_service.get_stock_price(int_ticker)
+        assert False
+    except InputNotString as e:
+        assert str(e) == input_must_be_string
 
 
 def test_get_stock_price_ticker_empty_string():
