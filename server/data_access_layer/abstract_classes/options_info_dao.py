d025e89443c9e67fc570072eb05fd18d193ade5f
Alejandro F <afuste7@gmail.com>
2022-04-23 21:40:39 -0400

    removed dividend methods from options file

--- server/data_access_layer/abstract_classes/options_info_dao.py
+++ server/data_access_layer/abstract_classes/options_info_dao.py
@@ -17,42 +17,4 @@ class OptionsInfoDAO(ABC):
     def get_puts(self, ticker: str) -> pandas:
         pass
 
-    @abstractmethod
-    def get_tickers_dow(self) -> list:
-        pass
-
-    @abstractmethod
-    def get_tickers_nasdaq(self) -> list:
-        pass
-
-    @abstractmethod
-    def get_tickers_sp500(self) -> list:
-        pass
-
-    @abstractmethod
-    def get_all_dividends(self, ticker: str) -> pandas:
-        pass
-
-    @abstractmethod
-    def get_dividends_for_specific_period(self, ticker: str, start_date: str, end_date: str) -> pandas:
-        pass
-
-    @abstractmethod
-    def get_dividends_previous_year(self, ticker: str) -> pandas:
-        pass
-
-    @abstractmethod
-    def get_dividends_current_year(self, ticker: str) -> pandas:
-        pass
-
-    @abstractmethod
-    def get_targeted_dividends(self, tickers: list) -> list:
-        pass
-
-    @abstractmethod
-    def get_dow_targets(self) -> list:
-        pass
-
-    @abstractmethod
-    def get_dividend_investment_amount(self, tickers: list, stock_price: float, investment: float) -> list:
-        pass
+   
