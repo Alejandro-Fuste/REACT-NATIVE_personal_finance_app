fcfc10b5a94e4bcfc6062c48f75d29ad11b1193f
Alejandro F <afuste7@gmail.com>
2022-04-30 16:04:58 -0400

    added entity folder and options entity test file

---
+++ server/tests/entities/test_options_entity.py
@@ -0,0 +1,37 @@
+from server.entities.option import Option
+
+
+def test_create_option_id():
+    pass
+
+
+def test_calculate_call_breakeven_amount():
+    pass
+
+
+def test_calculate_call_breakeven_percent():
+    pass
+
+
+def test_calculate_put_breakeven_amount():
+    pass
+
+
+def test_calculate_put_breakeven_percent():
+    pass
+
+
+def test_calculate_straddle_call_breakeven_amount():
+    pass
+
+
+def test_calculate_straddle_call_breakeven_percent():
+    pass
+
+
+def test_calculate_straddle_put_breakeven_amount():
+    pass
+
+
+def test_calculate_straddle_put_breakeven_percent():
+    pass
