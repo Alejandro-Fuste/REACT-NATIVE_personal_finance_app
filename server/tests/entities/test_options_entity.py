ca1f92267e5893af05a78e173404ad4d41864ede
Alejandro F <afuste7@gmail.com>
2022-04-30 16:35:54 -0400

    added code to option entity tests...all tests passed

--- server/tests/entities/test_options_entity.py
+++ server/tests/entities/test_options_entity.py
@@ -1,37 +1,49 @@
 from server.entities.option import Option
 
+test_call_option = Option("T", 19.00, 18.94, "call", "5/6", "straddle", .25, .38)
+option = test_call_option.make_dictionary()
+
 
 def test_create_option_id():
-    pass
+    option_id = option["optionId"]
+    assert isinstance(option_id, int)
 
 
 def test_calculate_call_breakeven_amount():
-    pass
+    amount = option["callBreakevenAmount"]
+    assert amount == 19.25
 
 
 def test_calculate_call_breakeven_percent():
-    pass
+    amount = option["callBreakevenPercent"]
+    assert amount == 1.64
 
 
 def test_calculate_put_breakeven_amount():
-    pass
+    amount = option["putBreakevenAmount"]
+    assert amount == 18.62
 
 
 def test_calculate_put_breakeven_percent():
-    pass
+    amount = option["putBreakevenPercent"]
+    assert amount == 1.69
 
 
 def test_calculate_straddle_call_breakeven_amount():
-    pass
+    amount = option["straddleCallBreakevenAmount"]
+    assert amount == 19.63
 
 
 def test_calculate_straddle_call_breakeven_percent():
-    pass
+    amount = option["straddleCallBreakevenPercent"]
+    assert amount == 3.64
 
 
 def test_calculate_straddle_put_breakeven_amount():
-    pass
+    amount = option["straddlePutBreakevenAmount"]
+    assert amount == 18.37
 
 
 def test_calculate_straddle_put_breakeven_percent():
-    pass
+    amount = option["straddlePutBreakevenPercent"]
+    assert amount == 3.01
