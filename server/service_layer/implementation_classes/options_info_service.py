48f484b34137811308281e34bb745f8c331fb79a
Alejandro F <afuste7@gmail.com>
2022-05-07 16:37:40 -0400

    fixed import error in service file

--- server/service_layer/implementation_classes/options_info_service.py
+++ server/service_layer/implementation_classes/options_info_service.py
@@ -3,7 +3,7 @@ import pandas
 from server.custom_exceptions.input_not_string import InputNotString
 from server.custom_exceptions.input_missing import InputMissing
 
-from server.data_access_layer.implementation_classes.options_info_dao import OptionsInfoImp
+from server.data_access_layer.implementation_classes.options_info_dao import OptionsInfoDAOImp
 from server.service_layer.abstract_classes.options_info_service_abs import OptionsInfoService
 
 input_must_be_string: str = "The input must be a string."
@@ -12,7 +12,7 @@ input_not_provided: str = "An input must be provided."
 
 class OptionsInfoServiceImp(OptionsInfoService):
     def __init__(self, options_info_dao):
-        self.options_info_dao: OptionsInfoImp = options_info_dao
+        self.options_info_dao: OptionsInfoDAOImp = options_info_dao
 
     def get_stock_price(self, ticker: str) -> float:
         # check ticker not blank
